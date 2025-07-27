8ebc0dba87ecc75b455250380d415b5fa1a38e2c
Eric <chen200778@gmail.com>
2025-07-27 22:54:04 +0200

    correzione blank

--- src/app/(root)/ReviewsSection/ReviewsSection.tsx
+++ src/app/(root)/ReviewsSection/ReviewsSection.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import styles from './ReviewsSection.module.scss';
 import { FaStar } from 'react-icons/fa';
@@ -20,8 +18,8 @@ export default function ReviewsSection() {
             <p className="medium mt-regular"><FaStar className={styles.icon} /> 4.8 / 5 su oltre 900 recensioni tra <a href={socials.facebook} className="simple-link-2">Facebook</a> e <a href={socials.googleMaps} className="simple-link-2">Google Maps</a></p>
           </div>
           <div className={styles.logos}>
-            <a href="https://www.tripadvisor.it/LocationPhotoDirectLink-g1079927-d2372529-i181379313-Trattoria_San_Bernardo-Morimondo_Province_of_Milan_Lombardy.html" target='_blanc'><Image src='/trip-advisor-logo.png' alt='trip-advisor' width={134} height={56} /></a>
-            <a href="https://restaurantguru.it/Trattoria-San-Bernardo-Morimondo" target='_blanc'><Image src='/restaurant-guru-logo.webp' alt='trip-advisor' width={56} height={56} /></a>
+            <a href="https://www.tripadvisor.it/LocationPhotoDirectLink-g1079927-d2372529-i181379313-Trattoria_San_Bernardo-Morimondo_Province_of_Milan_Lombardy.html" target='_blank'><Image src='/trip-advisor-logo.png' alt='trip-advisor' width={134} height={56} /></a>
+            <a href="https://restaurantguru.it/Trattoria-San-Bernardo-Morimondo" target='_blank'><Image src='/restaurant-guru-logo.webp' alt='trip-advisor' width={56} height={56} /></a>
           </div>
         </div>
         <Carousel items={
